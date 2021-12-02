bdb6e30a2f105eedf886e82d7e0dd3fb98a321a2
Wan-Ze <Wan-Ze@ITR4B653EW>
2021-12-02 11:16:05 +0800

    加入專案檔案。

---
+++ ListBoxDragDrop/SetWorkDay.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace ListBoxDragDrop
+{
+    partial class SetWorkDay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstOrderWorkDayPerson = new System.Windows.Forms.ListBox();
+            this.btnAddWorkDayPerson = new System.Windows.Forms.Button();
+            this.btnDeleteWorkDayPerson = new System.Windows.Forms.Button();
+            this.txtWorkDayPerson = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstOrderWorkDayPerson
+            // 
+            this.lstOrderWorkDayPerson.FormattingEnabled = true;
+            this.lstOrderWorkDayPerson.ItemHeight = 12;
+            this.lstOrderWorkDayPerson.Location = new System.Drawing.Point(12, 38);
+            this.lstOrderWorkDayPerson.Name = "lstOrderWorkDayPerson";
+            this.lstOrderWorkDayPerson.Size = new System.Drawing.Size(157, 256);
+            this.lstOrderWorkDayPerson.TabIndex = 0;
+            this.lstOrderWorkDayPerson.DragDrop += new System.Windows.Forms.DragEventHandler(this.lstOrderWorkDayPerson_DragDrop);
+            this.lstOrderWorkDayPerson.DragOver += new System.Windows.Forms.DragEventHandler(this.lstOrderWorkDayPerson_DragOver);
+            this.lstOrderWorkDayPerson.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lstOrderWorkDayPerson_MouseDown);
+            // 
+            // btnAddWorkDayPerson
+            // 
+            this.btnAddWorkDayPerson.Location = new System.Drawing.Point(103, 9);
+            this.btnAddWorkDayPerson.Name = "btnAddWorkDayPerson";
+            this.btnAddWorkDayPerson.Size = new System.Drawing.Size(66, 23);
+            this.btnAddWorkDayPerson.TabIndex = 1;
+            this.btnAddWorkDayPerson.Text = "新增人員";
+            this.btnAddWorkDayPerson.UseVisualStyleBackColor = true;
+            this.btnAddWorkDayPerson.Click += new System.EventHandler(this.btnAddWorkDayPerson_Click);
+            // 
+            // btnDeleteWorkDayPerson
+            // 
+            this.btnDeleteWorkDayPerson.Location = new System.Drawing.Point(72, 300);
+            this.btnDeleteWorkDayPerson.Name = "btnDeleteWorkDayPerson";
+            this.btnDeleteWorkDayPerson.Size = new System.Drawing.Size(97, 23);
+            this.btnDeleteWorkDayPerson.TabIndex = 2;
+            this.btnDeleteWorkDayPerson.Text = "刪除點選人員";
+            this.btnDeleteWorkDayPerson.UseVisualStyleBackColor = true;
+            this.btnDeleteWorkDayPerson.Click += new System.EventHandler(this.btnDeleteWorkDayPerson_Click);
+            // 
+            // txtWorkDayPerson
+            // 
+            this.txtWorkDayPerson.Location = new System.Drawing.Point(12, 9);
+            this.txtWorkDayPerson.Name = "txtWorkDayPerson";
+            this.txtWorkDayPerson.Size = new System.Drawing.Size(85, 22);
+            this.txtWorkDayPerson.TabIndex = 3;
+            // 
+            // SetWorkDay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 450);
+            this.Controls.Add(this.txtWorkDayPerson);
+            this.Controls.Add(this.btnDeleteWorkDayPerson);
+            this.Controls.Add(this.btnAddWorkDayPerson);
+            this.Controls.Add(this.lstOrderWorkDayPerson);
+            this.Name = "SetWorkDay";
+            this.Text = "SetWorkDay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstOrderWorkDayPerson;
+        private System.Windows.Forms.Button btnAddWorkDayPerson;
+        private System.Windows.Forms.Button btnDeleteWorkDayPerson;
+        private System.Windows.Forms.TextBox txtWorkDayPerson;
+    }
+}
---
+++ ListBoxDragDrop/SetWorkDay.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxDragDrop
+{
+    public partial class SetWorkDay : Form
+    {
+        public SetWorkDay()
+        {
+            InitializeComponent();
+            this.lstOrderWorkDayPerson.AllowDrop = true;
+        }
+
+        private void btnAddWorkDayPerson_Click(object sender, EventArgs e)
+        {
+            if (txtWorkDayPerson.Text!="")
+            {
+                lstOrderWorkDayPerson.Items.Add((lstOrderWorkDayPerson.Items.Count+1).ToString()+"_"+txtWorkDayPerson.Text);
+            }
+            txtWorkDayPerson.Text = "";
+        }
+
+        private void btnDeleteWorkDayPerson_Click(object sender, EventArgs e)
+        {
+
+            if (lstOrderWorkDayPerson.SelectedIndex!=-1)
+            {
+                lstOrderWorkDayPerson.Items.RemoveAt(lstOrderWorkDayPerson.SelectedIndex);
+            }
+            
+        }
+
+        private void lstOrderWorkDayPerson_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (lstOrderWorkDayPerson.SelectedItem==null)
+            {
+                return;
+            }
+            lstOrderWorkDayPerson.DoDragDrop(lstOrderWorkDayPerson.SelectedItem,DragDropEffects.Move);
+
+        }
+
+        private void lstOrderWorkDayPerson_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void lstOrderWorkDayPerson_DragDrop(object sender, DragEventArgs e)
+        {
+            Point point = lstOrderWorkDayPerson.PointToClient(new Point(e.X,e.Y));
+            int index = lstOrderWorkDayPerson.IndexFromPoint(point);
+
+            if (index<0)
+            {
+                //如果沒有選到就是最後一項
+                index = lstOrderWorkDayPerson.Items.Count - 1;
+                
+            }
+            object data = e.Data.GetData(typeof(string));
+            lstOrderWorkDayPerson.Items.Remove(data);
+            lstOrderWorkDayPerson.Items.Insert(index, data);
+            ReSetOrder();
+        }
+
+        public void ReSetOrder() 
+        {
+            if (lstOrderWorkDayPerson.Items.Count>0)
+            {
+                for (int i = 0; i < lstOrderWorkDayPerson.Items.Count; i++)
+                {
+                    lstOrderWorkDayPerson.Items[i] = (i+1).ToString()+"_"+lstOrderWorkDayPerson.Items[i].ToString().Split('_')[1];
+                }
+            }
+        
+        }
+    }
+}
